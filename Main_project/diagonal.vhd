05af413b5f550da273c62c4d690b84b59ffe3b68
RasmusBruhn <rasmusbruhnnielsen@gmail.com>
2021-06-04 12:17:25 +0200

    Wrote comments to all diagonals and straight, not the test benches

--- Main_project/diagonal.vhd
+++ Main_project/diagonal.vhd
@@ -11,58 +11,58 @@ entity diagonal is
     port (
         clock_100           : in  std_logic;
         
-        layer_1             : in  std_logic_vector(7 downto 0);
-        layer_2             : in  std_logic_vector(7 downto 0);
-		layer_3				: in  std_logic_vector(7 downto 0);
+        layer_1             : in  std_logic_vector(7 downto 0); -- The first layer of LED's '1' if LED is on
+        layer_2             : in  std_logic_vector(7 downto 0); -- The second layer of LED's '1' if LED is on
+		layer_3				: in  std_logic_vector(7 downto 0); -- The third layer of LED's '1' if LED is on
 		
-		reset               : in std_logic;
-		start_comparison    : in std_logic;
+		reset               : in std_logic; -- Button which resets system when pressed
+		start_comparison    : in std_logic; -- Switch which needs to be on for comparisons to occur
 
-        Found_match            : out std_logic := '0'
+        Found_match         : out std_logic := '0' -- Whether a match was found or not
     );
 end diagonal;
 
 architecture behav of diagonal is
 	
-	signal ready            : std_logic                       := '0';
-	signal output_right     : std_logic_vector(7 downto 0)    := (others => '0');
-	signal output_left      : std_logic_vector(7 downto 0)    := (others => '0');
+	signal ready            : std_logic                       := '0'; -- '1' when the FPGA should look for tracks
+	signal output_right     : std_logic_vector(7 downto 0)    := (others => '0'); -- Where on the board it found right diagonal tracks
+	signal output_left      : std_logic_vector(7 downto 0)    := (others => '0'); -- Where on the board it found left diagonal tracks
 
 begin
 
-    ready_proc: process(clock_100)
+    ready_proc: process(clock_100) -- The process to check if it should be comparing
     begin
         if rising_edge(clock_100) then
-			if reset = '1' then
+			if reset = '1' then -- If it is reseting then it should not be comparing
 				ready <= '0';
-			else
+			else -- If it is not resetting then the behavior is determined by start_comparison
 				ready <= start_comparison;
 			end if;
 		end if;
 	end process;
 	
-    comp_proc: process(clock_100)
+    comp_proc: process(clock_100) -- The main process in which comparisons are made
     begin
         if rising_edge(clock_100) then
-			if reset = '1' then
+			if reset = '1' then -- If it is resetting then set all values to default
 				output_left 	<= (others => '0');
 				output_right 	<= (others => '0');
 				Found_match <= '0';
-			elsif ready = '1' then
-				for i in 7 downto 0 loop
-					if i <= 5 then
+			elsif ready = '1' then -- If it is ready then look for tracks
+				for i in 7 downto 0 loop -- Look at all positions on the board 
+					if i <= 5 then -- If we are not at the last 2 positions then look for a right diagonal
                         output_right(i) <= layer_1(i) and layer_2(i+1) and layer_3(i+2);
-                    else 
+                    else -- If we are at one of the last 2 positions then a diagonal right is impossible since there is not space
                         output_right(i) <= '0';
                     end if;
-                    if i>=2 then
+                    if i>=2 then -- If we are not at the first 2 positions then look for a left diagonal
                         output_left(i) <= layer_1(i) and layer_2(i-1) and layer_3(i-2);
-                    else 
+                    else -- If we are at one of the first 2 positions then a diagonal left is impossible since there is not space
                         output_left(i) <= '0';
                     end if;
 				end loop;
-				Found_match <= or_reduce(output_left) or or_reduce(output_right);
-			else
+				Found_match <= or_reduce(output_left) or or_reduce(output_right); -- Check if it found a match
+			else -- If it is not ready then also reset to default values
                 output_left <= (others => '0');
                 output_right <= (others => '0');
 				Found_match <= '0';
--- Main_project/digonaltwo.vhd
+++ Main_project/digonaltwo.vhd
@@ -11,51 +11,51 @@ entity diagonal is
     port (
         clock_100           : in  std_logic;
         
-        layer_1             : in  std_logic_vector(7 downto 0);
-        layer_2             : in  std_logic_vector(7 downto 0);
-		layer_3				: in  std_logic_vector(7 downto 0);
+        layer_1             : in  std_logic_vector(7 downto 0); -- The first layer of LED's '1' if LED is on
+        layer_2             : in  std_logic_vector(7 downto 0); -- The second layer of LED's '1' if LED is on
+		layer_3				: in  std_logic_vector(7 downto 0); -- The third layer of LED's '1' if LED is on
 
-        Found_match            : out std_logic := '0'
+        Found_match         : out std_logic := '0' -- Whether it found a track or not
     );
 end diagonal;
 
 architecture behav of diagonal is
 	
-	signal output_right       : std_logic_vector(7 downto 0)    := (others => '0');
-    signal output_left        : std_logic_vector(7 downto 0)    := (others => '0');
+	signal output_right       : std_logic_vector(7 downto 0)    := (others => '0'); -- Where on the board it found right diagonal tracks
+    signal output_left        : std_logic_vector(7 downto 0)    := (others => '0'); -- Where on the board it found left diagonal tracks
 
 begin
 
-    process(clock_100)
+    process(clock_100) -- Main process where it looks for tracks
     begin
         if rising_edge(clock_100) then
-            for i in 7 downto 0 loop
-                if (layer_1(i) and layer_2(i)) then
-                    if 7-i>=1 then
+            for i in 7 downto 0 loop -- Loop through all of the possible positions
+                if (layer_1(i) and layer_2(i)) then -- Look for a bend track
+                    if 7-i>=1 then -- if it is not at the last position look for a right bend
 				        output_right(i) <= layer_2(i) and layer_3(i+1);
 				    end if;
-                    if i>=1 then
+                    if i>=1 then -- if it is not at the first position look for a left bend
                         output_left(i) <= layer_2(i) and layer_3(i-1);
                     end if;
                 end if;
-                if 7-i>=1 then
+                if 7-i>=1 then -- If it is not at the last position look for a right fork or a right diagonal
                     if (layer_1(i) and layer_2(i+1)) then
-                        if 7-i>=2 then
+                        if 7-i>=2 then -- if there is space for a right diagonal look for it
                             output_right(i) <= (layer_2(i+1) and layer_3(i+2)) or output_right(i);
                         end if;
-                        output_right(i) <= (layer_2(i+1) and layer_3(i+1)) or output_right(i);
+                        output_right(i) <= (layer_2(i+1) and layer_3(i+1)) or output_right(i); -- look for a right fork
                     end if;
                 end if;
-                if i>=1 then
+                if i>=1 then -- if it is not at the first position look for a left fork
                     if (layer_1(i) and layer_2(i+1)) then
-                        if i>=2 then
+                        if i>=2 then -- if there is space for a left diagonal look for it
                             output_left(i) <= (layer_2(i-1) and layer_3(i-2)) or output_left(i);
                         end if;
-                        output_left(i) <= (layer_2(i-1) and layer_3(i-1)) or output_left(i);
+                        output_left(i) <= (layer_2(i-1) and layer_3(i-1)) or output_left(i); -- look for a left fork
                 end if;
 			end loop;
 			
-			Found_match <= or_reduce(output_left) or or_reduce(output_right);
+			Found_match <= or_reduce(output_left) or or_reduce(output_right); -- Check whether it found a match
         end if;
     end process;
 
